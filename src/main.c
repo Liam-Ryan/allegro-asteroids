e5bcb6779b86a331ddd30f59e066e7d2375d0308
Liam Ryan <liamryandev@gmail.com>
2017-09-29 19:56:34 +0100

    Allegro bouncing block example

--- src/main.c
+++ src/main.c
@@ -3,10 +3,21 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
  
+const float FPS = 60;
+const float BOUNCER_SIZE = 32;
 
 int main(int argc, char **argv) {
 	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	ALLEGRO_DISPLAY_MODE disp_data;
+	ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_BITMAP *bouncer = NULL;
+	float bouncer_x = 0;
+	float bouncer_y = 0;
+	float bouncer_dir_x = -4.0;
+	float bouncer_dir_y = 4.0;
+	bool redraw = true;
+       	
 
 	if(!al_init() || !al_init_image_addon() || !al_init_primitives_addon()) {
 		fprintf(stderr, "Failed to initialize allegro!\n");
@@ -15,14 +26,80 @@ int main(int argc, char **argv) {
 
 	al_get_display_mode(al_get_num_display_modes() - 1, &disp_data);
 
-	al_set_new_display_flags(ALLEGRO_FULLSCREEN);
+//	al_set_new_display_flags(ALLEGRO_FULLSCREEN);
 	display = al_create_display(disp_data.width, disp_data.height);
 	if(!display) {
 		fprintf(stderr, "Failed to create display");
 		return -1;
 	}
+
+	bouncer_x = disp_data.width / 2.0 - BOUNCER_SIZE / 2.0;
+	bouncer_y = disp_data.height / 2.0 - BOUNCER_SIZE / 2.0;
+
+	bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
+	if(!bouncer) {
+		fprintf(stderr, "Failed to create bitmap!\n");
+		al_destroy_display(display);
+		al_destroy_timer(timer);
+		return -1;
+	}
+
+	al_set_target_bitmap(bouncer);
+	al_clear_to_color(al_map_rgb(255,0,255));
+	al_set_target_bitmap(al_get_backbuffer(display));
+
+	timer = al_create_timer (1.0 / FPS);
+	if(!timer) {
+		fprintf(stderr, "Failed to create timer!\n");
+	}
 	
-	al_rest(2.0);
+	event_queue = al_create_event_queue();
+	if(!event_queue) {
+		fprintf(stderr, "Failed to create event queue!\n");
+		al_destroy_bitmap(bouncer);
+		al_destroy_display(display);
+		al_destroy_timer(timer);
+		return -1;
+	}
+
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+	al_clear_to_color(al_map_rgb(0,0,0));
+	al_flip_display();
+	al_start_timer(timer);
+
+	while(1) {
+		ALLEGRO_EVENT ev;
+
+		al_wait_for_event(event_queue, &ev);
+
+		if(ev.type == ALLEGRO_EVENT_TIMER){
+			if(bouncer_x < 0 || bouncer_x > disp_data.width - BOUNCER_SIZE) 
+				bouncer_dir_x = -bouncer_dir_x;
+			if(bouncer_y < 0 || bouncer_y > disp_data.height - BOUNCER_SIZE)
+				bouncer_dir_y = -bouncer_dir_y;
+			
+			bouncer_x += bouncer_dir_x;
+			bouncer_y += bouncer_dir_y;
+
+			redraw = true;
+		}
+		else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			break;
+		}
+
+		if(redraw && al_is_event_queue_empty(event_queue)) {
+			redraw = false;
+			al_clear_to_color(al_map_rgb(0,0,0));
+			al_draw_bitmap(bouncer, bouncer_x, bouncer_y, 0);
+			al_flip_display();
+		}
+		
+	}
+
+	al_destroy_bitmap(bouncer);
+	al_destroy_timer(timer);
 	al_destroy_display(display);
+	al_destroy_event_queue(event_queue);
 	return 0;
 }
