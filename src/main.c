d3afaf83c0ef115ba3d740b2d9065163caf4c8be
Liam Ryan <liamryandev@gmail.com>
2017-09-29 17:50:32 +0100

    Added cmake build for including image and primitive add-ons. Implemented allegro example for full-screen

--- src/main.c
+++ src/main.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+ 
 
 int main(int argc, char **argv) {
 	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_DISPLAY_MODE disp_data;
 
-	if(!al_init()) {
+	if(!al_init() || !al_init_image_addon() || !al_init_primitives_addon()) {
 		fprintf(stderr, "Failed to initialize allegro!\n");
 		return -1;
 	}
 
-	display = al_create_display(640, 480);
+	al_get_display_mode(al_get_num_display_modes() - 1, &disp_data);
+
+	al_set_new_display_flags(ALLEGRO_FULLSCREEN);
+	display = al_create_display(disp_data.width, disp_data.height);
 	if(!display) {
 		fprintf(stderr, "Failed to create display");
 		return -1;
 	}
-	al_clear_to_color(al_map_rgb(0,0,0));
-	al_flip_display();
-	al_rest(10.0);
+	
+	al_rest(2.0);
 	al_destroy_display(display);
 	return 0;
 }
